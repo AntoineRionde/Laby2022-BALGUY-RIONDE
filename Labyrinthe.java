5a47d3316ac7e7e13c9c937558c035b00ab69c01
toinou887 <bosstoinou@gmail.com>
2022-03-21 11:22:33 +0100

    Update Labyrinthe.java

---
+++ Labyrinthe.java
@@ -0,0 +1,19 @@
+/**
+ * Labyrinthe
+ */
+public class Labyrinthe {
+
+    private final static char MUR = 'X';
+    private final static char PJ = 'P';
+    private final static char SORTIE = 'S';
+    private final static char VIDE = '.';
+
+    private final static String haut = "HAUT";
+    private final static String bas = "BAS";
+    private final static String gauche = "GAUCHE";
+    private final static String droite = "DROITE";
+
+
+
+
+}
